6f8132f81b6970d4a80190c35dbb50822bcdc100
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-08-10 13:36:06 +0200

    Initial commit

---
+++ lib/logic_gates.ex
@@ -0,0 +1,18 @@
+defmodule LogicGates do
+  @moduledoc """
+  Documentation for `LogicGates`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LogicGates.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logic_gates_test.exs
@@ -0,0 +1,8 @@
+defmodule LogicGatesTest do
+  use ExUnit.Case
+  doctest LogicGates
+
+  test "greets the world" do
+    assert LogicGates.hello() == :world
+  end
+end
