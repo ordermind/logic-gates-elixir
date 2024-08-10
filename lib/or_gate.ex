84461b43f64eb34a600f9cd0330ef214b7d5fc1b
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-08-10 18:53:30 +0200

    Create tests for OR gate

--- lib/or_gate.ex
+++ lib/or_gate.ex
@@ -1,4 +1,42 @@
-defmodule OrGate do
+defmodule LogicGates.OrGate do
+  @doc ~S"""
+  Executes an OR gate on an input list. The list may contain either boolean values or anonymous functions that return
+  either :ok and a boolean, or :error and a reason string or atom.
+
+  An OR gate returns true if at least one of the input values is true. For performance reasons the values are evaluated
+  sequentially and the execution stops as soon as a `true` value is found. This means that any errors later in the list
+  are not found:
+
+  iex> LogicGates.OrGate.exec([true, :error])
+  {:ok, :true}
+
+  ## Truth table with three input values
+
+      iex> LogicGates.OrGate.exec([false, false, false])
+      {:ok, :false}
+
+      iex> LogicGates.OrGate.exec([false, false, true])
+      {:ok, :true}
+
+      iex> LogicGates.OrGate.exec([false, true, false])
+      {:ok, :true}
+
+      iex> LogicGates.OrGate.exec([false, true, true])
+      {:ok, :true}
+
+      iex> LogicGates.OrGate.exec([true, false, false])
+      {:ok, :true}
+
+      iex> LogicGates.OrGate.exec([true, false, true])
+      {:ok, :true}
+
+      iex> LogicGates.OrGate.exec([true, true, false])
+      {:ok, :true}
+
+      iex> LogicGates.OrGate.exec([true, true, true])
+      {:ok, :true}
+
+  """
   @spec exec(list()) :: {:ok, boolean()} | {:error, binary()}
   def exec(input)
 
@@ -18,17 +56,18 @@ defmodule OrGate do
         exec(tail)
 
       {:error, reason} ->
-        {:error, reason}
+        {:error,
+         "Error in LogicGates.OrGate.exec/1: an error was returned by a function input value: #{inspect(reason)}"}
 
       other ->
         {:error,
-         "When a function is passed as a value to an OR gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(other)}"}
+         "Error in LogicGates.OrGate.exec/1: when a function is passed as an input value to an OR gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(other)}"}
     end
   end
 
   def exec([head | _]) do
     {:error,
-     "Each input value for an OR gate must be either a function or a boolean. Current value: #{inspect(head)}"}
+     "Error in LogicGates.OrGate.exec/1: each input value for an OR gate must be either a function or a boolean. Current value: #{inspect(head)}"}
   end
 
   def exec([]) do
@@ -36,6 +75,7 @@ defmodule OrGate do
   end
 
   def exec(input) do
-    {:error, "The value of an OR gate must be a list. Current value: #{inspect(input)}"}
+    {:error,
+     "Error in LogicGates.OrGate.exec/1: the value of an OR gate must be a list. Current value: #{inspect(input)}"}
   end
 end
--- test/or_gate_test.exs
+++ test/or_gate_test.exs
@@ -1,6 +1,7 @@
 defmodule OrGateTest do
   use ExUnit.Case
-  doctest OrGate
+  doctest LogicGates.OrGate
+  alias LogicGates.OrGate
 
   test "exec/1 returns error on invalid input type" do
     [
@@ -15,7 +16,7 @@ defmodule OrGateTest do
     |> Enum.each(fn invalid_input ->
       assert OrGate.exec(invalid_input) == {
                :error,
-               "The value of an OR gate must be a list. Current value: #{inspect(invalid_input)}"
+               "Error in LogicGates.OrGate.exec/1: the value of an OR gate must be a list. Current value: #{inspect(invalid_input)}"
              }
     end)
   end
@@ -32,7 +33,7 @@ defmodule OrGateTest do
     |> Enum.each(fn invalid_value ->
       assert OrGate.exec([invalid_value]) == {
                :error,
-               "Each input value for an OR gate must be either a function or a boolean. Current value: #{inspect(invalid_value)}"
+               "Error in LogicGates.OrGate.exec/1: each input value for an OR gate must be either a function or a boolean. Current value: #{inspect(invalid_value)}"
              }
     end)
   end
@@ -50,12 +51,45 @@ defmodule OrGateTest do
     |> Enum.each(fn invalid_return_value ->
       assert OrGate.exec([fn -> invalid_return_value end]) == {
                :error,
-               "When a function is passed as a value to an OR gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(invalid_return_value)}"
+               "Error in LogicGates.OrGate.exec/1: when a function is passed as an input value to an OR gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(invalid_return_value)}"
              }
     end)
   end
 
+  test "exec/1 returns an error when an error is return from a function input value" do
+    assert OrGate.exec([fn -> {:error, "Test error"} end]) ==
+             {:error,
+              "Error in LogicGates.OrGate.exec/1: an error was returned by a function input value: \"Test error\""}
+  end
+
   test "exec/1 returns false on empty list input" do
     assert OrGate.exec([]) == {:ok, false}
+  end
+
+  test "exec/1 boolean input" do
+    # This is covered by the doctests
+  end
+
+  test "exec/1 function input" do
+    [
+      {[false, false, false], {:ok, false}},
+      {[false, false, true], {:ok, true}},
+      {[false, true, false], {:ok, true}},
+      {[false, true, true], {:ok, true}},
+      {[true, false, false], {:ok, true}},
+      {[true, false, true], {:ok, true}},
+      {[true, true, false], {:ok, true}},
+      {[true, true, true], {:ok, true}}
+    ]
+    |> Enum.each(fn {return_values, expected_output} ->
+      assert OrGate.exec(
+               Enum.map(
+                 return_values,
+                 fn return_value ->
+                   fn -> {:ok, return_value} end
+                 end
+               )
+             ) == expected_output
+    end)
   end
 end
