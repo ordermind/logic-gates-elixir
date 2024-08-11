069385d0ee025cfc1ae36b77d80c16aa89e79c13
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-08-11 17:55:52 +0200

    Add ex_doc as dev dependency

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule LogicGates.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.3", only: [:test], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
