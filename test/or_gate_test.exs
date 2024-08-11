c4fd2406557ba1ee1213b5907c3cf506a94ed505
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-08-11 08:19:35 +0200

    Fix formatting issues

--- test/or_gate_test.exs
+++ test/or_gate_test.exs
@@ -14,10 +14,11 @@ defmodule OrGateTest do
       nil
     ]
     |> Enum.each(fn invalid_input ->
-      assert OrGate.exec(invalid_input) == {
-               :error,
-               "Error in LogicGates.OrGate.exec/1: the value of an OR gate must be a list. Current value: #{inspect(invalid_input)}"
-             }
+      assert(
+        OrGate.exec(invalid_input) ==
+          {:error,
+           "Error in LogicGates.OrGate.exec/1: the value of an OR gate must be a list. Current value: #{inspect(invalid_input)}"}
+      )
     end)
   end
 
@@ -31,10 +32,11 @@ defmodule OrGateTest do
       nil
     ]
     |> Enum.each(fn invalid_value ->
-      assert OrGate.exec([invalid_value]) == {
-               :error,
-               "Error in LogicGates.OrGate.exec/1: each input value for an OR gate must be either a function or a boolean. Current value: #{inspect(invalid_value)}"
-             }
+      assert(
+        OrGate.exec([invalid_value]) ==
+          {:error,
+           "Error in LogicGates.OrGate.exec/1: each input value for an OR gate must be either a function or a boolean. Current value: #{inspect(invalid_value)}"}
+      )
     end)
   end
 
@@ -49,17 +51,20 @@ defmodule OrGateTest do
       nil
     ]
     |> Enum.each(fn invalid_return_value ->
-      assert OrGate.exec([fn -> invalid_return_value end]) == {
-               :error,
-               "Error in LogicGates.OrGate.exec/1: when a function is passed as an input value to an OR gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(invalid_return_value)}"
-             }
+      assert(
+        OrGate.exec([fn -> invalid_return_value end]) ==
+          {:error,
+           "Error in LogicGates.OrGate.exec/1: when a function is passed as an input value to an OR gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(invalid_return_value)}"}
+      )
     end)
   end
 
   test "exec/1 returns an error when an error is return from a function input value" do
-    assert OrGate.exec([fn -> {:error, "Test error"} end]) ==
-             {:error,
-              "Error in LogicGates.OrGate.exec/1: an error was returned by a function input value: \"Test error\""}
+    assert(
+      OrGate.exec([fn -> {:error, "Test error"} end]) ==
+        {:error,
+         "Error in LogicGates.OrGate.exec/1: an error was returned by a function input value: \"Test error\""}
+    )
   end
 
   test "exec/1 returns false on empty list input" do
@@ -82,14 +87,16 @@ defmodule OrGateTest do
       {[true, true, true], {:ok, true}}
     ]
     |> Enum.each(fn {return_values, expected_output} ->
-      assert OrGate.exec(
-               Enum.map(
-                 return_values,
-                 fn return_value ->
-                   fn -> {:ok, return_value} end
-                 end
-               )
-             ) == expected_output
+      assert(
+        OrGate.exec(
+          Enum.map(
+            return_values,
+            fn return_value ->
+              fn -> {:ok, return_value} end
+            end
+          )
+        ) == expected_output
+      )
     end)
   end
 end
