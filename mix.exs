fab89eb6d4a9b2b722e61ec0ff3251093e571031
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-08-11 17:32:11 +0200

    Use specific github actions settings for dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule LogicGates.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        # Put the project-level PLT in the priv/ directory (instead of the default _build/ location)
+        plt_file: {:no_warn, "priv/plts/project.plt"}
+
+        # The above is equivalent to:
+        # plt_local_path: "priv/plts/project.plt"
+
+        # You could also put the core Erlang/Elixir PLT into the priv/ directory like so:
+        # plt_core_path: "priv/plts/core.plt"
+      ]
     ]
   end
 
