a0d186bbd43932e47d3c6d5c75383c07c5d23fc9
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-08-11 13:20:47 +0200

    Add AND gate with tests

---
+++ lib/and.ex
@@ -0,0 +1,93 @@
+defmodule LogicGates.And do
+  @doc ~S"""
+  Executes an AND gate on an input list. The list may contain either boolean values or anonymous functions that return
+  either :ok and a boolean, or :error and a reason string or atom.
+
+  An AND gate returns true if all of the input values evaluate to true. Otherwise it returns false.
+
+  For performance reasons the input values are evaluated sequentially and the execution stops as soon as a `false` value
+  is found. This means that any errors later in the list are not found:
+
+  iex> LogicGates.And.exec([false, fn -> {:error, "Test error"} end])
+  {:ok, :false}
+
+  ## Truth table with three input values
+
+      iex> LogicGates.And.exec([false, false, false])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([false, false, true])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([false, true, false])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([false, true, true])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([true, false, false])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([true, false, true])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([true, true, false])
+      {:ok, :false}
+
+      iex> LogicGates.And.exec([true, true, true])
+      {:ok, :true}
+
+  """
+  @spec exec(list()) :: {:ok, boolean()} | {:error, binary()}
+  def exec(input)
+
+  def exec([head | tail]) do
+    exec_recursive([head | tail])
+  end
+
+  def exec([]) do
+    {:error,
+     "Error in LogicGates.And.exec/1: the function was called with an empty list as parameter, so there are no input values to evaluate."}
+  end
+
+  def exec(input) do
+    {:error,
+     "Error in LogicGates.And.exec/1: the parameter to the function must be a list. Current parameter: #{inspect(input)}"}
+  end
+
+  defp exec_recursive(input)
+
+  defp exec_recursive([head | tail]) when is_boolean(head) do
+    case head do
+      true -> exec_recursive(tail)
+      false -> {:ok, false}
+    end
+  end
+
+  defp exec_recursive([head | tail]) when is_function(head) do
+    case head.() do
+      {:ok, true} ->
+        exec_recursive(tail)
+
+      {:ok, false} ->
+        {:ok, false}
+
+      {:error, reason} ->
+        {:error,
+         "Error in LogicGates.And.exec/1: an error was returned by a function input value: #{inspect(reason)}"}
+
+      other ->
+        {:error,
+         "Error in LogicGates.And.exec/1: when a function is passed as an input value to an AND gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(other)}"}
+    end
+  end
+
+  defp exec_recursive([head | _]) do
+    {:error,
+     "Error in LogicGates.And.exec/1: each input value for an AND gate must be either a function or a boolean. Current value: #{inspect(head)}"}
+  end
+
+  defp exec_recursive([]) do
+    {:ok, true}
+  end
+end
---
+++ test/and_test.exs
@@ -0,0 +1,184 @@
+defmodule AndTest do
+  use ExUnit.Case
+  doctest LogicGates.And
+  alias LogicGates.And
+
+  test "exec/1 returns error on invalid parameter type" do
+    [
+      true,
+      "false",
+      %{true: false},
+      {true, false},
+      0,
+      1.0,
+      nil
+    ]
+    |> Enum.each(fn invalid_input ->
+      assert(
+        And.exec(invalid_input) ==
+          {:error,
+           "Error in LogicGates.And.exec/1: the parameter to the function must be a list. Current parameter: #{inspect(invalid_input)}"}
+      )
+    end)
+  end
+
+  test "exec/1 returns error on invalid type of value in input list" do
+    [
+      "false",
+      %{true: false},
+      {true, false},
+      0,
+      1.0,
+      nil
+    ]
+    |> Enum.each(fn invalid_value ->
+      assert(
+        And.exec([invalid_value]) ==
+          {:error,
+           "Error in LogicGates.And.exec/1: each input value for an AND gate must be either a function or a boolean. Current value: #{inspect(invalid_value)}"}
+      )
+    end)
+  end
+
+  test "exec/1 returns error on invalid return type of function value" do
+    [
+      true,
+      "false",
+      %{true: false},
+      {true, false},
+      0,
+      1.0,
+      nil
+    ]
+    |> Enum.each(fn invalid_return_value ->
+      assert(
+        And.exec([fn -> invalid_return_value end]) ==
+          {:error,
+           "Error in LogicGates.And.exec/1: when a function is passed as an input value to an AND gate, it must return a tuple consisting of either :ok and a boolean, or :error and a string. Returned value: #{inspect(invalid_return_value)}"}
+      )
+    end)
+  end
+
+  test "exec/1 returns an error when an error is return from a function input value" do
+    assert(
+      And.exec([true, fn -> {:error, "Test error"} end]) ==
+        {:error,
+         "Error in LogicGates.And.exec/1: an error was returned by a function input value: \"Test error\""}
+    )
+  end
+
+  test "exec/1 returns an error on empty list input" do
+    assert(
+      And.exec([]) ==
+        {:error,
+         "Error in LogicGates.And.exec/1: the function was called with an empty list as parameter, so there are no input values to evaluate."}
+    )
+  end
+
+  test "exec/1 boolean 1 input value" do
+    [
+      {[false], {:ok, false}},
+      {[true], {:ok, true}}
+    ]
+    |> Enum.each(fn {input_values, expected_output} ->
+      assert(And.exec(input_values) == expected_output)
+    end)
+  end
+
+  test "exec/1 boolean 2 input values" do
+    [
+      {[false, false], {:ok, false}},
+      {[false, true], {:ok, false}},
+      {[true, false], {:ok, false}},
+      {[true, true], {:ok, true}}
+    ]
+    |> Enum.each(fn {input_values, expected_output} ->
+      assert(And.exec(input_values) == expected_output)
+    end)
+  end
+
+  test "exec/1 boolean 3 input values" do
+    # This is covered by the doctests
+  end
+
+  test "exec/1 function 1 input value" do
+    [
+      {[false], {:ok, false}},
+      {[true], {:ok, true}}
+    ]
+    |> Enum.each(fn {return_values, expected_output} ->
+      assert(
+        And.exec(
+          Enum.map(
+            return_values,
+            fn return_value ->
+              fn -> {:ok, return_value} end
+            end
+          )
+        ) == expected_output
+      )
+    end)
+  end
+
+  test "exec/1 function 2 input values" do
+    [
+      {[false, false], {:ok, false}},
+      {[false, true], {:ok, false}},
+      {[true, false], {:ok, false}},
+      {[true, true], {:ok, true}}
+    ]
+    |> Enum.each(fn {return_values, expected_output} ->
+      assert(
+        And.exec(
+          Enum.map(
+            return_values,
+            fn return_value ->
+              fn -> {:ok, return_value} end
+            end
+          )
+        ) == expected_output
+      )
+    end)
+  end
+
+  test "exec/1 function 3 input values" do
+    [
+      {[false, false, false], {:ok, false}},
+      {[false, false, true], {:ok, false}},
+      {[false, true, false], {:ok, false}},
+      {[false, true, true], {:ok, false}},
+      {[true, false, false], {:ok, false}},
+      {[true, false, true], {:ok, false}},
+      {[true, true, false], {:ok, false}},
+      {[true, true, true], {:ok, true}}
+    ]
+    |> Enum.each(fn {return_values, expected_output} ->
+      assert(
+        And.exec(
+          Enum.map(
+            return_values,
+            fn return_value ->
+              fn -> {:ok, return_value} end
+            end
+          )
+        ) == expected_output
+      )
+    end)
+  end
+
+  test "exec/1 mixed input value types" do
+    [
+      {[false, fn -> {:ok, false} end, false], {:ok, false}},
+      {[false, fn -> {:ok, false} end, true], {:ok, false}},
+      {[false, fn -> {:ok, true} end, false], {:ok, false}},
+      {[false, fn -> {:ok, true} end, true], {:ok, false}},
+      {[true, fn -> {:ok, false} end, false], {:ok, false}},
+      {[true, fn -> {:ok, false} end, true], {:ok, false}},
+      {[true, fn -> {:ok, true} end, false], {:ok, false}},
+      {[true, fn -> {:ok, true} end, true], {:ok, true}}
+    ]
+    |> Enum.each(fn {input_values, expected_output} ->
+      assert(And.exec(input_values) == expected_output)
+    end)
+  end
+end
