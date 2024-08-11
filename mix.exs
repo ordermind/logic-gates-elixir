70a851fb0e92ff29b12c537b5f4593924daa05dc
Kristofer Tengström <ordermind@gmail.com>
2024-08-11 16:44:06 +0000

    new version 0.3.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LogicGates.MixProject do
     [
       app: :logic_gates,
       name: "Logic Gates",
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: description(),
